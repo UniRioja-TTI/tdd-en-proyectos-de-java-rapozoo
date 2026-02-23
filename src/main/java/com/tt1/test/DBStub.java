d97b93c4eaf2ab2f204bd5dcc3b6852dc952902a
rapozoo <113707095+rapozoo@users.noreply.github.com>
2026-02-23 20:56:58 +0100

    Hecho hasta mediados del ejercicio 4

---
+++ src/main/java/com/tt1/test/DBStub.java
@@ -0,0 +1,20 @@
+package com.tt1.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DBStub {
+	public static Map<Integer, ToDo> listaToDo = new HashMap<>();
+	public static ToDo create(ToDo o) {
+		throw  new  UnsupportedOperationException("Clase  aún  no  implementada.");
+	}
+	public static ToDo read(ToDo o) {
+		throw  new  UnsupportedOperationException("Clase  aún  no  implementada.");
+	}
+	public static ToDo update(ToDo o) {
+		throw  new  UnsupportedOperationException("Clase  aún  no  implementada.");
+	}
+	public static ToDo remove(ToDo o) {
+		throw  new  UnsupportedOperationException("Clase  aún  no  implementada.");
+	}
+}
---
+++ src/main/java/com/tt1/test/ToDo.java
@@ -0,0 +1,50 @@
+package com.tt1.test;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ToDo implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String nombre;
+	private String descripcion;
+	private Date fecha;
+	private boolean completado;
+	
+	public ToDo() {
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public boolean isCompletado() {
+		return completado;
+	}
+
+	public void setCompletado(boolean completado) {
+		this.completado = completado;
+	}
+}
